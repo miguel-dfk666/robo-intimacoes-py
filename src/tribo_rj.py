f43e5bae7d79a0467ab063c9a2a03ea74aec7624
Miguel Ozana <107505846+miguel-dfk666@users.noreply.github.com>
2023-12-18 11:05:54 -0300

    .

--- src/tribo_rj.py
+++ src/tribo_rj.py
@@ -23,55 +23,60 @@ class Pesquisa:
         }
 
 def main():
-  navegador = Navegador()
-  driver = navegador.iniciar_chrome()
-  
-  navegador.driver.get('https://www3.tjrj.jus.br/idserverjus-front/#/login?indGet=true&sgSist=PORTALSERVICOS')
-  navegador.driver.find_element(By.XPATH, '//*[@id="iniciodoconteudo"]/div[1]/form/div/div[2]/div/div[2]/div/div/div[2]/a').click()
-  janelas_abertas = navegador.driver.window_handles
+    navegador = Navegador()
+    driver = navegador.iniciar_chrome()
 
-  navegador.driver.switch_to(janelas_abertas[1])
-  wait = WebDriverWait(navegador.driver, 20)
-  input_element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="dropdownPerfil"]/div/div[1]/div[1]/input')))
-  input_element.send_keys('Advogado')
-  pyautogui.press('enter')
-  time.sleep(0.7)
-  navegador.driver.execute_script("javascript:void(0)")
-  print("Usuário Logado")
-  time.sleep(5)
+    processador = ProcessadorDeProcessos(driver)
+    pesquisa = Pesquisa()
+
+    site_rj = 'https://www3.tjrj.jus.br/idserverjus-front/#/login?indGet=true&sgSist=PORTALSERVICOS'
+
+    navegador.driver.get(site_rj)
+    navegador.driver.find_element(By.XPATH, '//*[@id="iniciodoconteudo"]/div[1]/form/div/div[2]/div/div[2]/div/div/div[2]/a').click()
+    janelas_abertas = navegador.driver.window_handles
+
+    navegador.driver.switch_to(janelas_abertas[1])
+    wait = WebDriverWait(navegador.driver, 20)
+    input_element = wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="dropdownPerfil"]/div/div[1]/div[1]/input')))
+    input_element.send_keys('Advogado')
+    pyautogui.press('enter')
+    time.sleep(0.7)
+    navegador.driver.execute_script("javascript:void(0)")
+    print("Usuário Logado")
+    time.sleep(5)
 
 
-  # Ações antes da consulta de processos, necessário fazer scrap na página
-  navegador.driver.find_element(By.XPATH, '//*[@id="PORTLET"]').click()
-  consulta_intimacao = WebDriverWait(navegador.driver, 40).until(EC.presence_of_element_located((By.XPATH, '//*[@id="corpo"]/app-menu-expandido/section/div/div[2]/div[4]/div')))
-  consulta_intimacao.click()
-  time.sleep(2)
-  input_place = WebDriverWait(navegador.driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="filtroStatus"]')))
-  input_place.send_keys('Recebidas', Keys.ARROW_DOWN)
-  pyautogui.press('enter')
-  time.sleep(2)
+    # Ações antes da consulta de processos, necessário fazer scrap na página
+    navegador.driver.find_element(By.XPATH, '//*[@id="PORTLET"]').click()
+    consulta_intimacao = WebDriverWait(navegador.driver, 40).until(EC.presence_of_element_located((By.XPATH, '//*[@id="corpo"]/app-menu-expandido/section/div/div[2]/div[4]/div')))
+    consulta_intimacao.click()
+    time.sleep(2)
+    input_place = WebDriverWait(navegador.driver, 20).until(EC.presence_of_element_located((By.XPATH, '//*[@id="filtroStatus"]')))
+    input_place.send_keys('Recebidas', Keys.ARROW_DOWN)
+    pyautogui.press('enter')
+    time.sleep(2)
 
-  navegador.driver.find_element(By.XPATH, '//*[@id="botaoPesquisarIntimacoes"]').click()
+    navegador.driver.find_element(By.XPATH, '//*[@id="botaoPesquisarIntimacoes"]').click()
 
-  # scrap
-  response = requests.get(site_rj)
-  if response.status_code == 200:
-      html_content = response.txt
-      
-      soup = BeautifulSoup(html_content, 'html.parser')
-      
-      processo_element = soup.select("(//td[contains(@class,'ng-tns-c405-2')])[3]")
-      
-      for element in processo_element:
-          text = element.get_text(strip=True)
-          print(f"Numero Processo: {text}")
-  else:
-      print('Falha ao processar página: ', response.status_code)
+    # scrap
+    response = requests.get(site_rj)
+    if response.status_code == 200:
+        html_content = response.txt
+        
+        soup = BeautifulSoup(html_content, 'html.parser')
+        
+        processo_element = soup.select("(//td[contains(@class,'ng-tns-c405-2')])[3]")
+        
+        for element in processo_element:
+            text = element.get_text(strip=True)
+            print(f"Numero Processo: {text}")
+    else:
+        print('Falha ao processar página: ', response.status_code)
 
-  # Consultar processo
-  navegador.driver.find_element(By.XPATH, '//*[@id="CONSULTAS"]').click()
-  consulta_processo = WebDriverWait(navegador.driver, 40).until(EC.presence_of_element_located((By.XPATH, '//*[@id="corpo"]/app-menu-expandido/section/div/div[2]/div[1]/div')))
-  consulta_processo.click()
+    # Consultar processo
+    navegador.driver.find_element(By.XPATH, '//*[@id="CONSULTAS"]').click()
+    consulta_processo = WebDriverWait(navegador.driver, 40).until(EC.presence_of_element_located((By.XPATH, '//*[@id="corpo"]/app-menu-expandido/section/div/div[2]/div[1]/div')))
+    consulta_processo.click()
   
 if __name__ == '__main__':
     while True:
